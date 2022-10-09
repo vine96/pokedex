40a7818b8dfe69f55af04db8b0ac5c8df7a3c234
Vinícius Furtado Pecci de Lima <viniciuspecci@hotmail.com>
2022-10-09 14:40:07 -0300

    Definição inicial da consulta com a API

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,56 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
+    <v-container>
+      <v-card>
+        <v-container>
+          <v-row>
+            <v-col cols=3 
+            v-for="pokemon in pokemons" 
+            :key="pokemon.name">
+            <h2>{{ pokemon.name }}</h2>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import axios from 'axios';
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
+  components: {},
+
+  data() {
+    return {
+      pokemons: []
+    }
   },
 
-  data: () => ({
-    //
-  }),
+  mounted() {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151").then((response) => {
+      this.pokemons = response.data.results;
+    });
+  },
 };
 </script>
+
+<style>
+#app {
+  background: linear-gradient(
+      to bottom right,
+      rgba(10, 10, 10, 1),
+      rgba(12, 39, 63, 1)
+    )
+    no-repeat center center fixed !important;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover !important;
+  background-position: center;
+  min-height: 100vh;
+}
+</style>
